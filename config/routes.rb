3162b78e493a00784fa627afec0fd89d96df1ecd
Anthony Chao <qoosuperman@gmail.com>
2019-09-19 13:58:34 +0800

    create groups controller and routes before import bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :groups do
+    collection do
+      get :my
+      get :attend
+      get :public
+    end
+  end
+
 end
