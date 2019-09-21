dc8cd1e08752acfce1a4a2584156e072c7a27416
Anthony Chao <qoosuperman@gmail.com>
2019-09-21 12:56:01 +0800

    fill  my_groups column & create groups controller new/create detail

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -5,16 +5,27 @@ class GroupsController < ApplicationController
   end
 
   def my
+    #這裏之後要改掉 User.first
+    @current_user= User.first
+    @groups = @current_user.groups
+  end
+
+  def attend
+    # @current_user= User.first
+    # @groups = @current_user.groups.orders.find_by(buyer_id: @current_user.id)
   end
 
   def new
-    @group = Group.new
+    #  #這裏之後要改掉 User.first
+    @group = User.first.groups.new
+    @group.products.build
   end
 
   def create
-    
-    clean_params = params.require(:groups).permit(:title, :description, :address, :phone, :is_active, :start_time, :end_time, :is_public, products_attributes:[:name, :price, :_destroy], options_attributes:[:name, :price, :_destroy])
-    @group = Group.new(clean_params)
+
+    clean_params = params.require(:group).permit(:title, :description, :address, :phone, :is_active, :start_time, :end_time, :is_public, products_attributes:[:name, :price, :_destroy], options_attributes:[:name, :price, :_destroy])
+     #這裏之後要改掉 User.first
+    @group = User.first.groups.new(clean_params)
     if @group.save
       redirect_to my_groups_path
     else
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,2 +1,4 @@
 class OrderItem < ApplicationRecord
+  belongs_to :product
+  belongs_to :order
 end
