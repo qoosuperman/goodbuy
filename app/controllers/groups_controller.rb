afcf81b05b964137b172839d0a09fc27d34b86d7
Anthony Chao <qoosuperman@gmail.com>
2019-09-28 23:42:32 +0800

    結單更新  end_time /  結單之後不能買和編輯 / 只有團主可以編輯跟結單 / 做 flash notice

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,10 +1,9 @@
 class GroupsController < ApplicationController
   before_action :authenticate_user!
   before_action :find_group, only: [:edit, :update, :close, :link, :buy, :show, :checkout]
+  before_action :validate_group_owner, only: [:edit, :close]
+  before_action :validate_group_active, only: [:buy, :edit]
   
-
-  require 'rqrcode'
-
   def index
   end
 
@@ -53,7 +52,7 @@ class GroupsController < ApplicationController
   end
 
   def close
-    @group.update(is_active: false)
+    @group.update(is_active: false, end_time: Time.now)
     redirect_to my_groups_path
   end
 
@@ -92,5 +91,16 @@ class GroupsController < ApplicationController
     clean_params = params.require(:group).permit(:title, :description, :address, :phone, :is_active, :start_time, :end_time, :is_public, :shop_photo, products_attributes:[:id, :name, :price, :_destroy], options_attributes:[:id, :name, :price, :_destroy])
   end
 
+  def validate_group_owner
+    if @group.user_id != current_user.id
+      redirect_to my_groups_path, notice: "只有團主可以編輯/結單喔！"
+    end
+  end
+
+  def validate_group_active
+    unless @group.is_active 
+      redirect_to my_groups_path, notice: "這個團已經結單囉！"
+    end
+  end
 
 end
