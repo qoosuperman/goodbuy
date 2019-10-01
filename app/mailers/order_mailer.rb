ee747c08aa34d09aaa7c75dbd34bdff946479996
Anthony Chao <qoosuperman@gmail.com>
2019-10-01 17:42:19 +0800

    add column trade_no to orders

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,2 +1,5 @@
 class OrderMailer < ApplicationMailer
+  def order_confirm
+    mail(to: @order.buyer.email, subject: "您的訂單#{@order.trade_no}")
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_27_062209) do
+ActiveRecord::Schema.define(version: 2019_10_01_093912) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -84,6 +84,7 @@ ActiveRecord::Schema.define(version: 2019_09_27_062209) do
     t.bigint "group_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "trade_no"
     t.index ["group_id"], name: "index_orders_on_group_id"
   end
 
