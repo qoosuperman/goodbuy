bc7d878cc2a87127642ea6ebedfd9f71c8973aea
chai <wown0710@gmail.com>
2019-10-04 16:46:39 +0800

    buy RWD

--- app/assets/javascripts/buy/buy.js
+++ app/assets/javascripts/buy/buy.js
@@ -1,6 +1,6 @@
 $(function () {
 
-  var $li = $('ul.tab-title li');
+  let $li = $('ul.tab-title li');
   $($li.eq(0).addClass('active').find('a').attr('href')).siblings('.tab-inner').hide();
 
   $li.click(function (e) {
@@ -11,13 +11,14 @@ $(function () {
 
 
   let countPrice = function () {
-    let productMoney = $("#tab-demo .products .product input[type=radio]:checked").val();
+    let productMoney = $(".products .product input[type=radio]:checked").val();
     let total = Number(productMoney) || 0;
-    $("#tab-demo .options .option input[type=checkbox]:checked").each(function () {
+    $(".options .option input[type=checkbox]:checked").each(function () {
       let optMoney = (Number(this.value) || 0);
       total = total + optMoney;
     });
     $('.total-price').text(`${total}元`);
+    $('.total-price2').text(`${total}元`);
 
   };
 
@@ -64,6 +65,45 @@ $(function () {
 
   }
 
+  let showItem2 = function () {
+
+    let product = $("#tab-demo .products .product input[type=radio]:checked").parent(".product:eq(0)").text()
+
+    let options = $("#tab-demo .options .option input[type=checkbox]:checked").parent(".option").text()
+
+    let productId = $("#tab-demo .products .product input[type=radio]:checked").parent(".product:eq(0)").data('product')
+
+    let optionId = []
+    countChecked = $("#tab-demo .options .option input[type=checkbox]:checked").length
+
+    for (let i = 0; i < countChecked; i++) {
+      optionId.push($("#tab-demo .options .option input[type=checkbox]:checked").parent(`.option:eq(${i})`).data('option')
+      )
+    }
+
+    let total = $('.total-price2').text()
+
+
+    let tpl = `
+      <div class="id-${orderItemId} pb-3 lh-125 border-bottom border-gray order-item">
+        <strong class="text-gray-dark">品名：${product}</strong>
+        <span class="d-block">選項：${options}</span>
+        <span class="d-block">價格：${total}</span>
+        <input type="hidden" name="item[][product]" value="${productId}" />
+        <input type="hidden" name="item[][options]" value="${optionId}" />
+        <button class='delete mt-2 btn btn-delete btn-sm'>刪除囉</button>
+      </div>
+    `
+
+    $("#order-item2").append(tpl)
+    orderItemId = orderItemId + 1
+
+    $('input').prop('checked', false)
+
+    $('.total-price').text(`${0}元`)
+
+  }
+
   // $("#buy").click(showItem)
   // $("#buy").on('click', () => showItem)
 
@@ -73,8 +113,22 @@ $(function () {
     alert("已加入購買清單");
   });
 
+  $("#buy2").on('click', function (e) {
+    e.preventDefault()
+    showItem2()
+    alert("已加入購買清單");
+  });
 
-  $('#tab02 #order-item').on('click', '.delete', function (e) {
+
+  $('#order-item').on('click', '.delete', function (e) {
+    e.preventDefault();
+    var $this = $(this),
+      $parent = $this.parents('.order-item:eq(0)');
+    var ans = confirm('確定刪掉嗎');
+    if (ans === true) $parent.remove();
+  });
+
+  $('#order-item2').on('click', '.delete', function (e) {
     e.preventDefault();
     var $this = $(this),
       $parent = $this.parents('.order-item:eq(0)');
