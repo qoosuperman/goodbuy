dba8e7b770f8926052abbf4d01e49d7e456ce719
Anthony Chao <qoosuperman@gmail.com>
2019-10-04 13:46:34 +0800

    switch dev storage to S3 & gem amazon SDK

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.active_storage.service = :local
+
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
