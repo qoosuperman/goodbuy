05a7836ea24c6809afbd1bd91b99a526bccbe6df
Anthony Chao <qoosuperman@gmail.com>
2019-10-06 17:36:10 +0800

    add products/groups/options validateion

--- app/models/group.rb
+++ app/models/group.rb
@@ -7,6 +7,7 @@ class Group < ApplicationRecord
   belongs_to :user
   has_one_attached :shop_photo
   has_one_attached :qr_img
+  validates :title, presence: { message: "這欄必填喔！" }
 
   def total_price
     orders.reduce(0){ |acc, order|
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
   belongs_to :group
   has_one :order_item
+  validates :name, :price, presence: { message: "這欄必填喔！" }
 end
