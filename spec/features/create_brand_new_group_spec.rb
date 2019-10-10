4e9219b1171ee2151173d86e581f3e52ed5401e6
Anthony Chao <qoosuperman@gmail.com>
2019-10-10 17:02:25 +0800

    add 新增全新團購 feature & add transfer gem due to test with js

---
+++ spec/features/create_brand_new_group_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.feature "CreateBrandNewGroups", type: :feature do
+  before do
+    user = create(:user)
+    sign_in user
+    visit new_group_path
+  end
+
+  scenario "可以進到新增團購頁面" do
+    expect(page).to have_content("來開團囉")
+  end
+
+  scenario "填入基本資訊" do
+    within(".form-pc") do
+      fill_in("group[title]", with: "團團圓圓")
+      click_button("確定開團")
+    end
+
+    visit my_groups_path
+    expect(page).to have_content("團團圓圓")
+  end
+
+  scenario "填入基本資訊加菜單", js: true do
+    within(".form-pc") do
+      fill_in("group[title]", with: "團團圓圓")
+      click_on("菜單")
+      click_on("新增菜單")
+      first('input[placeholder="奶茶"]').set("紅茶")
+      first('input[placeholder="50"]').set("20")
+      click_button("確定開團")
+    end
+    expect(page).to have_content("你的團購連結")
+  end
+end
--- spec/features/groups_spec.rb
+++
@@ -1,4 +0,0 @@
-require 'rails_helper'
-
-RSpec.feature "Groups", type: :feature do
-end
