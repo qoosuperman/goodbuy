5ebaed7f201a96490d4dacc0dc9217e35061ac2b
Anthony Chao <qoosuperman@gmail.com>
2019-10-13 18:53:35 +0800

    pc 上傳檔案可以跑出檔案名字 但是 phone 版還不行

--- app/assets/javascripts/groups_new/group_new.js
+++ app/assets/javascripts/groups_new/group_new.js
@@ -1,10 +1,23 @@
 $(function(){
   var $li = $('ul.tab-title li');
-      $($li. eq(0) .addClass('active').find('a').attr('href')).siblings('.tab-inner').hide();
+  $($li. eq(0) .addClass('active').find('a').attr('href')).siblings('.tab-inner').hide();
   
-      $li.click(function(e){
-          e.preventDefault();
-          $($(this).find('a'). attr ('href')).show().siblings ('.tab-inner').hide();
-          $(this).addClass('active'). siblings ('.active').removeClass('active');
-      });
+  $li.click(function(e){
+    e.preventDefault();
+    $($(this).find('a'). attr ('href')).show().siblings ('.tab-inner').hide();
+    $(this).addClass('active'). siblings ('.active').removeClass('active');
   });
+
+  $('.photo-upload-phone').change(function() {
+    var i = $(this).prev('label').clone();
+    var file = $('.photo-upload-phone')[0].files[0].name;
+    $(this).prev('label').text(file);
+  });
+
+  $('.photo-upload-pc').change(function() {
+    var i = $(this).prev('label').clone();
+    var file = $('.photo-upload-pc')[0].files[0].name;
+    $(this).prev('label').text(file);
+  });
+
+});
