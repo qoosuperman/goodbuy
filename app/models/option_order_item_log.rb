74c228545bf63070fd00e7ba5979a409a5a7504c
Anthony Chao <qoosuperman@gmail.com>
2020-02-29 13:15:55 +0800

    feature(model):add annotate in option_order_item model

--- app/models/option_order_item_log.rb
+++ app/models/option_order_item_log.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: option_order_item_logs
+#
+#  id            :bigint           not null, primary key
+#  option_id     :bigint
+#  order_item_id :bigint
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+
 class OptionOrderItemLog < ApplicationRecord
   belongs_to :option
   belongs_to :order_item
