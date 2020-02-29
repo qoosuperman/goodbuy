bb2c76556ed6c009e16d4836f5642841b8f444fb
Anthony Chao <qoosuperman@gmail.com>
2020-02-29 14:39:42 +0800

    feature(spec):add product spec

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: products
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  price      :integer
+#  group_id   :bigint
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Product < ApplicationRecord
   belongs_to :group
   has_one :order_item
--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: products
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  price      :integer
+#  group_id   :bigint
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 FactoryBot.define do
   factory :product do
     name { Faker::Food.dish }
